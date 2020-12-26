7afbd8e7284cd5479a0bbdde64befe272123dde1
Rahees Ahmed <raheesahmed256@gmail.com>
2020-12-26 12:06:52 +0400

    Initialize project using Create React App

---
+++ GridLayout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Grid.css";
+
+const GridLayout = () => {
+  return (
+    <div className="grid-wrappper">
+      <div className="div1"></div>
+      <div className="div2"></div>
+      <div className="div3"></div>
+      <div className="div4"></div>
+      <div className="div5"></div>
+      <div className="div6"></div>
+      <div className="div7"></div>
+      <div className="div8"></div>
+      <div className="div9"></div>
+      <div className="div10">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="div11"></div>
+    </div>
+  );
+};
+
+export default GridLayout;
